a8a5bdcfd5d85fc2168941803e70941a12335e2b
Alvaro Quena <alvaro.quena1@gmail.com>
2022-03-21 05:26:20 -0400

    Prepate Unit Test

---
+++ test/calculator_test.py
@@ -0,0 +1,16 @@
+class CalculatorTest(unittest.TestCase):
+    def test_add(self):
+        cal = Calculator()
+        result = cal.add(2, 2)
+        self.assertEqual(4, result)
+
+    def test
+    def test_validate_age(self):
+        cal = Calculator()
+        result = cal.valid_age(5)
+        self.assertTrue(result)
+
+    def test_validate_invalid_age(self):
+        cal = Calculator()
+        result = cal.valid_age(-5)
+        self.assertFalse(result)
